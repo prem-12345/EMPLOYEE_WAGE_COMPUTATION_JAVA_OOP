23d9d3c4b2b26022191f9211ec98f8d0241c00b8
prem-12345 <premkpatil1997@gmail.com>
2021-08-16 13:05:26 +0530

    WELCOME TO EMPLOYEE WAGE COMPUTATION OOP

---
+++ src/com/bridgelabz/empwagejavaoop/EmpWageMain.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.empwagejavaoop;
+
+public class EmpWageMain {
+    public static void main(String[] args) {
+
+        /* WELCOME TO EMPLOYEE WAGE COMPUTATION OOP */
+
+    }
+}
